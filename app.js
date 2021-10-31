71ad7e37401eeb3b07289ed5cc7709964e01c575
Krenil-Sapariya <sapariyakrenil@gmail.com>
2021-10-31 18:25:56 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const exhbs = require('express-handlebars');
+
+app.engine('hbs', exhbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
+
+// app.set('views', path.join(__dirname,'handlebars'));
+app.set('view engine', 'hbs');
+
+app.get('/', function (req, res) {
+    res.render('home', {
+        product: [
+            {
+                name: 'tv'
+                
+            }
+        ]
+    });
+});
+
+app.listen(5500, () => {
+    console.log('listening on 5500');
+})
